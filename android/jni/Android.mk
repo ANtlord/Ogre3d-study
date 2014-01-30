e4e0234bbb170f73ff7a15d6d4d52a514254f733
nvpopov <brodiaga38@gmail.com>
2014-01-30 17:50:04 +0900

    Dynamic source addition for preconf

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -3,41 +3,42 @@ LOCAL_PATH := $(call my-dir)
     LOCAL_MODULE    := ForumOgreProject
 
     LOCAL_LDLIBS	:= -landroid -lc -lm -ldl -llog -lEGL -lGLESv2
-    LOCAL_LDLIBS	+= -L/home/uantlord/Develop/ogre/ogre_clean_backup/build_android/lib \
-		-L/home/uantlord/Develop/ogre/ogre_clean_backup/AndroidDependencies/lib/armeabi-v7a
+    LOCAL_LDLIBS	+= -L/home/nvpopov/apps/ogre1_9/android_build/lib \
+		-L/home/nvpopov/apps/ogre1_9/AndroidDependencies/lib/armeabi
 
-    LOCAL_LDLIBS	+= -lPlugin_ParticleFXStatic -lPlugin_OctreeSceneManagerStatic -lRenderSystem_GLES2Static -lOgreRTShaderSystemStatic -lOgreMeshLodGeneratorStatic -lOgreOverlayStatic -lOgreTerrainStatic -lOgrePagingStatic -lOgreVolumeStatic -lOgreMainStatic
+    LOCAL_LDLIBS	+= -lPlugin_ParticleFXStatic -lPlugin_OctreeSceneManagerStatic -lRenderSystem_GLES2Static -lOgreRTShaderSystemStatic -lOgreOverlayStatic -lOgreTerrainStatic -lOgrePagingStatic -lOgreVolumeStatic -lOgreMainStatic
     LOCAL_LDLIBS	+= -lzzip -lz -lFreeImage -lfreetype -lOIS \
-					   /home/uantlord/Develop/ogre/ogre_clean_backup/build_android/systemlibs/armeabi-v7a/libsupc++.a \
-					   /home/uantlord/Develop/ogre/ogre_clean_backup/build_android/systemlibs/armeabi-v7a/libstdc++.a \
-					   /home/uantlord/Develop/ogre/ogre_clean_backup/build_android/SampleBrowserNDK/obj/local/armeabi-v7a/libcpufeatures.a
+					   /home/nvpopov/apps/ogre1_9/android_build/systemlibs/armeabi/libsupc++.a \
+					   /home/nvpopov/apps/ogre1_9/android_build/systemlibs/armeabi/libstdc++.a \
+					   /home/nvpopov/apps/ogre1_9/android_build/SampleBrowserNDK/obj/local/armeabi/libcpufeatures.a
     LOCAL_STATIC_LIBRARIES := android_native_app_glue cpufeatures
 
 	LOCAL_CFLAGS := -DGL_GLEXT_PROTOTYPES=1 \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/build_android/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/OgreMain/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/RenderSystems/GLES2/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/RenderSystems/GLES2/include/EGL
+		-I/home/nvpopov/apps/empty_ogre_project/include \
+		-I/home/nvpopov/apps/ogre1_9/android_build/include \
+		-I/home/nvpopov/apps/ogre1_9/OgreMain/include \
+		-I/home/nvpopov/apps/ogre1_9/RenderSystems/GLES2/include \
+		-I/home/nvpopov/apps/ogre1_9/RenderSystems/GLES2/include/EGL
 
-	LOCAL_CFLAGS += -I/opt/android-ndk/sources/cpufeatures \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/Components/RTShaderSystem/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/Components/MeshLodGenerator/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/Components/Overlay/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/Components/Volume/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/Components/Terrain/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/Components/Paging/include
+	LOCAL_CFLAGS += -I/home/nvpopov/apps/android-deps/android-ndk-r9c/sources/cpufeatures \
+		-I/home/nvpopov/apps/ogre1_9/Components/RTShaderSystem/include \
+		-I/home/nvpopov/apps/ogre1_9/Components/MeshLodGenerator/include \
+		-I/home/nvpopov/apps/ogre1_9/Components/Overlay/include \
+		-I/home/nvpopov/apps/ogre1_9/Components/Volume/include \
+		-I/home/nvpopov/apps/ogre1_9/Components/Terrain/include \
+		-I/home/nvpopov/apps/ogre1_9/Components/Paging/include
 
-	LOCAL_CFLAGS += -I/home/uantlord/Develop/ogre/ogre_clean_backup/PlugIns/ParticleFX/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/PlugIns/OctreeSceneManager/include
+	LOCAL_CFLAGS += -I/home/nvpopov/apps/ogre1_9/PlugIns/ParticleFX/include \
+		-I/home/nvpopov/apps/ogre1_9/PlugIns/OctreeSceneManager/include
 
-	LOCAL_CFLAGS += -I/home/uantlord/Develop/ogre/ogre_clean_backup/AndroidDependencies/include \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/AndroidDependencies/include/OIS \
-		-I/home/uantlord/Develop/ogre/ogre_clean_backup/build_android/Samples/include   
+	LOCAL_CFLAGS += -I/home/nvpopov/apps/ogre1_9/AndroidDependencies/include \
+		-I/home/nvpopov/apps/ogre1_9/AndroidDependencies/include/OIS \
+		-I/home/nvpopov/apps/ogre1_9/android_build/Samples/include   
 
 	LOCAL_CFLAGS += -fexceptions -frtti -x c++ -D___ANDROID___ -DANDROID -DZZIP_OMIT_CONFIG_H -DINCLUDE_RTSHADER_SYSTEM=1
 
-        LOCAL_PATH := /home/uantlord/Develop/ForumOgreProj/
-        LOCAL_SRC_FILES := main.cpp ShaderGeneratorTechniqueResolverListener.cpp
+        LOCAL_PATH := /home/nvpopov/apps/empty_ogre_project
+        LOCAL_SRC_FILES :=  src/main.cpp src/ShaderGeneratorTechniqueResolverListener.cpp 
 
     include $(BUILD_SHARED_LIBRARY)
 
