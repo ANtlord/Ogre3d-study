2a2559ddebbb3314cbeb6b0d82b4f373a52f00b2
antlord <antlord92@gmail.com>
2014-02-27 14:50:57 +0900

    triangulation fixed

--- src/gwTriangulate.cpp
+++ src/gwTriangulate.cpp
@@ -7,14 +7,27 @@ bool checkPoint(const double &x_a, const double &y_a,  const double &x_b,
         const double &y_b, const double &x_c, const double &y_c,
         const double &x_d, const double &y_d)
 {
+    // Move triangles to intersector of x-axis and y-axis for avoiding 
+    // constant b in linear equalization (y = kx + b)
+    const double X_DIFF = -x_a;
+    const double Y_DIFF = -y_a;
+    const double X0 = x_a + X_DIFF;
+    const double Y0 = y_a + Y_DIFF;
+    const double X1 = x_b + X_DIFF;
+    const double Y1 = y_b + Y_DIFF;
+    const double X2 = x_c + X_DIFF;
+    const double Y2 = y_c + Y_DIFF;
+    const double X3 = x_d + X_DIFF;
+    const double Y3 = y_d + Y_DIFF;
+
     // If a and b points makes up vertical line, then new and checking points
     // must be on different sites.
-    if (y_a==y_b && (y_d<y_a != y_c<y_a)) return true;
+    if (Y0==Y1 && (Y3<Y0 == Y2<Y0)) return true;
     // Similary for horizontal line.
-    if (x_a==x_b && (x_d<x_a != x_c<x_a)) return true;
+    if (X0==X1 && (X3<X0 == X2<X0)) return true;
     // For diagonal line.
-    const int K = (y_a / x_a);
-    if (y_d<K*x_d != y_c<K*x_c) return true;
+    const double K = (Y1 / X1);
+    if (Y3<K*X3 != Y2<K*X2) return true;
     return false;
 }
 
@@ -85,26 +98,28 @@ vector <int*> points_delaunay_naive_2d(int node_num, double node_xy[], int *tria
                                     bool c1 = k==tri[c][0] && j==tri[c][1];
                                     bool c2 = k==tri[c][0] && i==tri[c][2];
                                     bool c3 = j==tri[c][0] && i==tri[c][2];
+                                    // If 2 of 3 indices was saved, that check
+                                    // coordinates of third point.
                                     if (c1){
                                         isNotNewTriangle = checkPoint(
                                                 node_xy[0+k*2], node_xy[1+k*2],
-                                                node_xy[0+j*2], node_xy[0+j*2],
+                                                node_xy[0+j*2], node_xy[1+j*2],
                                                 node_xy[0+tri[c][2]*2], node_xy[1+tri[c][2]*2],
-                                                node_xy[0+i*2], node_xy[0+i*2]
+                                                node_xy[0+i*2], node_xy[1+i*2]
                                                 );
                                     }
                                     else if (c2){
                                         isNotNewTriangle = checkPoint(
                                                 node_xy[0+k*2], node_xy[1+k*2],
-                                                node_xy[0+i*2], node_xy[0+i*2],
+                                                node_xy[0+i*2], node_xy[1+i*2],
                                                 node_xy[0+tri[c][1]*2], node_xy[1+tri[c][1]*2],
-                                                node_xy[0+j*2], node_xy[0+j*2]
+                                                node_xy[0+j*2], node_xy[1+j*2]
                                                 );
                                     }
                                     else if (c3){
                                         isNotNewTriangle = checkPoint(
-                                                node_xy[0+i*2], node_xy[0+i*2],
-                                                node_xy[0+j*2], node_xy[0+j*2],
+                                                node_xy[0+i*2], node_xy[1+i*2],
+                                                node_xy[0+j*2], node_xy[1+j*2],
                                                 node_xy[0+tri[c][0]*2], node_xy[1+tri[c][0]*2],
                                                 node_xy[0+k*2], node_xy[1+k*2]
                                                 );
